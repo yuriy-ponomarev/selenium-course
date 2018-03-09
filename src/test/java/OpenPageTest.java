6b921e2454f5ffc3edc8817f6c4b4dcec1f67c2a
yuriy-ponomarev <yponomarev@outlook.com>
2018-03-09 17:54:36 +0300

    OpenPageTest

---
+++ src/test/java/OpenPageTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class OpenPageTest extends TestBase {
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+        wait.until(titleIs("Google"));
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class TestBase {
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        if (tlDriver.get() != null) {
+            driver = tlDriver.get();
+            wait = new WebDriverWait(driver, 10);
+            return;
+        }
+
+        driver = new ChromeDriver();
+        tlDriver.set(driver);
+        wait = new WebDriverWait(driver, 10);
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(() -> { driver.quit(); driver = null; }));
+    }
+
+    @AfterTest
+    public void stop() {
+    }
+}
