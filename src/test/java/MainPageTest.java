acdfbb302c5b5117b19206551c1029f06d042bdf
yuriy-ponomarev <yponomarev@outlook.com>
2018-04-10 00:13:01 +0300

    Task 8: Product locator fix

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -21,13 +21,13 @@ public class MainPageTest extends TestBase {
     @Test
     private void testMainPageProductSticker() {
         System.out.println("--- testMainPageProductSticker ---");
-        String allProductsLocator = "li[class='product column shadow hover-light']"; // for XPath: "//li[contains(@class, 'product')]" 
+        String allProductsLocator = "product"; // for XPath: "//li[contains(@class, 'product')]"
         String stickerLocator = "./a/div/div[contains(@class, 'sticker')]";
 
         driver.navigate().to(BASE_URL);
         wait.until(titleIs("Online Store | My Store"));
 
-        List<WebElement> productBoxes = driver.findElements(By.cssSelector(allProductsLocator));
+        List<WebElement> productBoxes = driver.findElements(By.className(allProductsLocator));
 
         productBoxes.forEach(e -> {
             List<WebElement> stickers = (e.findElements(By.xpath(stickerLocator)));
